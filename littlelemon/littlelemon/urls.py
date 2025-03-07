57de267296263f10621b1f4d532a77da931b4046
DominiqueDaquin <kouakepdominique@gmail.com>
2025-03-07 15:53:14 +0100

    djoser urls

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/menu/',include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
-
+    path('auth/',include("djoser.urls"))
 ]
