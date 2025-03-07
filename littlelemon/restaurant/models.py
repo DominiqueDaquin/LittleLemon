711adc844d56f03e5db70d595a21bda1a0905e19
DominiqueDaquin <kouakepdominique@gmail.com>
2025-03-07 15:50:39 +0100

    booking

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 class Category(models.Model):
     slug=models.SlugField()
     title=models.CharField(max_length=255,db_index=True)
@@ -7,3 +8,7 @@ class MenuItem(models.Model):
     price=models.DecimalField(max_digits=6,decimal_places=2,db_index=True)
     featured=models.BooleanField(db_index=True)
     category=models.ForeignKey(Category, on_delete=models.PROTECT, related_name='menu_items')
+
+class Booking(models.Model):
+    user=models.Foreignkey(User,on_delete=models.CASCADE)
+    menu=models.ForeignKey(MenuItem,on_delete=models.PROTECT)
